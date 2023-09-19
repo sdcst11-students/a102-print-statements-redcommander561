aca28ca5b549f3cb8f7e26926e9774ce7c1fb3eb
redcommander561 <126519219+redcommander561@users.noreply.github.com>
2023-09-19 12:45:28 -0700

    I made it super cool and awesome

--- assignment4.py
+++ assignment4.py
@@ -1,6 +1,7 @@
 #!python3
 
-Modify this program using comment structures so that
-only the print statement is executed.
+#Modify this program using comment structures so that
+#only the print statement is executed.
 
 print("Hello!")
+print ("How's it going?!?!??!!??!!??!")
